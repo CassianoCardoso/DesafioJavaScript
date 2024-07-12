f0711f9ef617184c711a87e9c088f3c1ff946201
CassianoCardoso <dj_kassiano@hotmail.com>
2024-07-12 18:26:21 -0300

    Desafio DIO

---
+++ index.js
@@ -0,0 +1,25 @@
+let nm ='Cassiano';
+let xp = 10001;
+
+
+if (xp <= 1000) {
+    console.log(` O heroi ${nm} esta no nível de xp ${xp} e ele é Ferro`)
+}
+else if (xp >= 1001 && xp  <= 2000) {
+    console.log(` O heroi ${nm} esta no nível de xp ${xp} e ele é Bronze`)
+}
+else if (xp >= 2001 && xp  <= 5000) {
+    console.log(` O heroi ${nm} esta no nível de xp${xp} e ele é Prata Ouro`)
+}
+else if (xp >= 5001 && xp  <= 8000) {
+    console.log(` O heroi ${nm} esta no nível de xp ${xp} e ele é Platina Diamante`)
+}
+else if (xp >= 8001 && xp  <= 9000) {
+    console.log(` O heroi ${nm} esta no nível de xp ${xp} e ele é Ascendente`)
+}
+else if (xp >= 9001 && xp  <= 10000) {
+    console.log(` O heroi ${nm} esta no nível de xp ${xp} e ele é Imortal`)
+}
+else (xp >= 10001); {
+    console.log(` O heroi ${nm} esta no nível de xp ${xp} e ele é Radiante`)
+}
